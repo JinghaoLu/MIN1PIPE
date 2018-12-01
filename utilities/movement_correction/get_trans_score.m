518b5b6ce97b38f0d883bdee9bc8a1aa63ee145d
JinghaoLu <6352026+JinghaoLu@users.noreply.github.com>
2018-12-01 12:53:46 -0500

    Update get_trans_score.m

--- utilities/movement_correction/get_trans_score.m
+++ utilities/movement_correction/get_trans_score.m
@@ -31,18 +31,22 @@ function [acorr, duse] = get_trans_score(Y, flag, ispara, isdisp, mq)
     duse = zeros(nframes - 1, 2);
     if nframes == 2 || ~ispara
         for i = 2: nframes
-            img_old = regt(:, :, i - 1);
-            img = Y(:, :, i);
-            d = klt2(normalize(img_old), normalize(img), biderr, mq);
-            duse(i - 1, :) = mean(d, 1);
-            if ~isempty(d)
-                if flag == 1
-                    acorr(i - 1) = mean(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2));
-                elseif flag == 2
-                    acorr(i - 1) = prctile(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2), 95);
+            try
+                img_old = regt(:, :, i - 1);
+                img = Y(:, :, i);
+                d = klt2(normalize(img_old), normalize(img), biderr, mq);
+                duse(i - 1, :) = mean(d, 1);
+                if ~isempty(d)
+                    if flag == 1
+                        acorr(i - 1) = mean(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2));
+                    elseif flag == 2
+                        acorr(i - 1) = prctile(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2), 95);
+                    end
+                else
+                    acorr(i - 1) = 100; %%% a large score %%%
                 end
-            else
-                acorr(i - 1) = 100; %%% a large score %%%
+            catch
+                acorr(i - 1) = NaN;
             end
             if isdisp
                 if mod(i, round(nframes / 10)) == 0
@@ -52,18 +56,22 @@ function [acorr, duse] = get_trans_score(Y, flag, ispara, isdisp, mq)
         end
     else
         parfor i = 2: nframes
-            img_old = regt(:, :, i - 1);
-            img = Y(:, :, i);
-            d = klt2(normalize(img_old), normalize(img), biderr, mq);
-            duse(i - 1, :) = mean(d, 1);
-            if ~isempty(d)
-                if flag == 1
-                    acorr(i - 1) = mean(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2));
+            try
+                img_old = regt(:, :, i - 1);
+                img = Y(:, :, i);
+                d = klt2(normalize(img_old), normalize(img), biderr, mq);
+                duse(i - 1, :) = mean(d, 1);
+                if ~isempty(d)
+                    if flag == 1
+                        acorr(i - 1) = mean(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2));
+                    else
+                        acorr(i - 1) = prctile(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2), 95);
+                    end
                 else
-                    acorr(i - 1) = prctile(sqrt(d(:, 1) .^ 2 + d(:, 2) .^ 2), 95);
+                    acorr(i - 1) = 100; %%% a large score %%%
                 end
-            else
-                acorr(i - 1) = 100; %%% a large score %%%
+            catch
+                acorr(i - 1) = NaN;
             end
             if isdisp
                 if mod(i, round(nframes / 10)) == 0
@@ -71,5 +79,11 @@ function [acorr, duse] = get_trans_score(Y, flag, ispara, isdisp, mq)
                 end
             end
         end
+    end
+    
+    if any(isnan(acorr))
+        ids = find(~isnan(acorr));
+        idss = find(isnan(acorr));
+        acorr(idss) = interp1(ids, acorr(ids), idss, 'pchip', 'extrap');
     end
 end
