b93562d3660f54989fe8b8464f725f1db8a94c5d
JinghaoLu <6352026+JinghaoLu@users.noreply.github.com>
2019-05-10 13:49:27 -0400

    Update data_info.m

--- utilities/elements/data_info.m
+++ utilities/elements/data_info.m
@@ -3,7 +3,7 @@ function [path_name, file_base, file_fmt] = data_info
 %   Jinghao Lu 01/12/2016
 
 %     [file_name_tmp, path_name] = uigetfile({'*.tif'; '*.tiff'; '*.avi'}, 'Select coordinates file', 'MultiSelect', 'on');
-    [file_name_tmp, path_name] = uigetfile('*', 'Select coordinates file', 'MultiSelect', 'on');
+    [file_name_tmp, path_name] = uigetfile('*', 'Select imaging file', 'MultiSelect', 'on');
     if ~iscell(file_name_tmp)
         file_name{1} = file_name_tmp;
     else
