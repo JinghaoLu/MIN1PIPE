9b06caa7b02442c2f0b12fbc0691d4f40b23b30b
JinghaoLu <6352026+JinghaoLu@users.noreply.github.com>
2019-10-31 09:59:44 -0400

    Update select_gpu.m

--- utilities/movement_correction/select_gpu.m
+++ utilities/movement_correction/select_gpu.m
@@ -1,14 +1,18 @@
 function select_gpu
     n = gpuDeviceCount;
     if n>0
-        s = zeros(1, n);
-        for i = 1: n
-            d = gpuDevice(i);
-            s(i) = d.AvailableMemory;
+        try
+            s = zeros(1, n);
+            for i = 1: n
+                d = gpuDevice(i);
+                s(i) = d.AvailableMemory;
+            end
+            [~, id] = max(s);
+            gpuDevice(id);
+            disp(['Using GPU Device #', num2str(id), ' Total Memory: ', num2str(s(id))])
+        catch
+            disp('GPU Device available but outdated.')
         end
-        [~, id] = max(s);
-        gpuDevice(id);
-        disp(['Using GPU Device #', num2str(id), ' Total Memory: ', num2str(s(id))])
     else
         disp('No GPU Device available.')
     end
