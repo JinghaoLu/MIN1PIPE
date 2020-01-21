174236c4f742e9c3f274600072b722991a37d976
JinghaoLu <6352026+JinghaoLu@users.noreply.github.com>
2020-01-21 12:11:42 -0500

    Update gradient_fast.m

--- utilities/movement_correction/gradient_fast.m
+++ utilities/movement_correction/gradient_fast.m
@@ -7,7 +7,7 @@ function [gx, gy] = gradient_fast(I)
     gy(1, :) = I(2, :) - I(1, :);
     gy(end, :) = I(end, :) - I(end - 1, :);
     gy(2: end - 1, :) = (I(3: end, :) - I(1: end - 2, :)) / 2;
-    gx(:, 1) = I(:, 1) - I(:, 1);
+    gx(:, 1) = I(:, 2) - I(:, 1);
     gx(:, end) = I(:, end) - I(:, end - 1);
     gx(:, 2: end - 1) = (I(:, 3: end) - I(:, 1: end - 2)) / 2;
 end
