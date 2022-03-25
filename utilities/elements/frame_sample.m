2621f7f8a0693b49b60b2d0a4a123faea706beb5
JinghaoLu <6352026+JinghaoLu@users.noreply.github.com>
2022-03-25 15:36:40 -0400

    Update frame_sample.m

--- utilities/elements/frame_sample.m
+++ utilities/elements/frame_sample.m
@@ -1,11 +1,25 @@
 function [se, spatialr] = frame_sample(m)
-    f = imfinfo(m);
-    n = length(f);
+    if contains(m, 'tif') || contains(m, 'tiff')
+        f = imfinfo(m);
+        n = length(f);
+    elseif contains(m, 'avi')
+        f = VideoReader;
+        n = floor(f.FrameRate * f.Duration);
+    elseif contains(m, 'mat')
+        f = matfile(m);
+        [pixh, pixw, n] = size(f.frame_all);
+    end
     nu = min(100, n);
     s = cell(1, nu);
     id = randsample(n, nu);
     for i = 1: nu
-        tmp = double(imread(m, id(i)));
+        if contains(m, 'tif') || contains(m, 'tiff')
+            tmp = double(imread(m, id(i)));
+        elseif contains(m, 'avi')
+            tmp = double(read(f, id(i)));
+        elseif contains(m, 'mat')
+            tmp = double(m.frame_all(1: pixh, 1: pixw, id(i)));
+        end
         st = auto_detect_params(tmp);
         s{i} = st;
     end
